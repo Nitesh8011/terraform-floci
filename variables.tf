29bec585cd31e871eb79cd06ae483afbbebabc5c
Nitesh <nitesh11041999@gmail.com>
2026-08-29 20:14:23 +0530

    default removed from root variable

--- variables.tf
+++ variables.tf
@@ -1,24 +1,20 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "module-s3-bucket"
 }
 
 variable "billing_mode" {
   description = "Dynamodb Billing mode"
   type        = string
-  default     = "PAY_PER_REQUEST"
 }
 
 variable "table_names" {
   type    = list(string)
-  default = ["module-table-1", "module-table-2", "module-table-3"]
 }
 
 variable "module_delay_seconds" {
