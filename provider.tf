60a649ff6870c035b2771a677f9e012545166979
Nitesh <nitesh11041999@gmail.com>
2026-08-29 19:24:56 +0530

    sqs in terraform

--- provider.tf
+++ provider.tf
@@ -36,5 +36,6 @@ provider "aws" {
   endpoints {
     s3  = "http://localhost:4566"
     sts = "http://localhost:4566"
+    sqs = "http://localhost:4566"
   }
 }
--- run.sh
+++ run.sh
@@ -10,14 +10,27 @@ step() {
 }
 
 step "FORMAT"
-terraform fmt
+terraform fmt -recursive
 step "FORMATTED"
+
+step "SELECT WORKSPACE"
+echo "Select the workspace to apply changes to:"
+select WORKSPACE in "staging" "production"; do
+  case "$WORKSPACE" in
+    staging|production) break ;;
+    *) echo "Invalid choice. Please select 1 or 2." ;;
+  esac
+done
+echo "Using workspace: $WORKSPACE"
+terraform workspace select "$WORKSPACE"
+
+VAR_FILE="${WORKSPACE}.tfvars"
 
 step "VALIDATE"
 terraform validate
 
 step "PLAN"
-terraform plan
+terraform plan -var-file="$VAR_FILE"
 
 step "APPLING"
-terraform apply
+terraform apply -var-file="$VAR_FILE"
